dd5be256ca385f460b118c6e5121e713039ff21c
user <user@user-desktop>
2010-04-05 17:57:47 +0200

    Dodata je forma za registraciju, koja za sad samo registruje da je kliknut submit i vadi podatke.

--- src/main/java/com/packtpub/celeb5/pages/Registration.java
+++ src/main/java/com/packtpub/celeb5/pages/Registration.java
@@ -1,5 +1,49 @@
 package com.packtpub.celeb5.pages;
 
 public class Registration {
+	
+	private String userName;
+	private String password;
+	private String password2;
+	private String gender;
+	
+	public String getUserName() {
+		return userName;
+	}
+	
+	public void setUserName(String userName) {
+		System.out.println("Setting user name: " + userName);
+		this.userName = userName;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		System.out.println("Setting password: " + password);
+		this.password = password;
+	}
+	
+	public String getPassword2() {
+		return password2;
+	}
+	
+	public void setPassword2(String password2) {
+		this.password2 = password2;
+	}
+	
+	public String getGender() {
+		return gender;
+	}
+	
+	public void setGender(String gender) {
+		System.out.println("Setting gender: " + gender);
+		this.gender = gender;
+	}
+
+	void onSubmitFromRegistrationForm(){
+		System.out.println("The form was submitted!");
+	}
 
 }
